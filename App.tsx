346aa2597a6c90482b6ed9aab1f325d256945c3d
Rafael Santos <rafa.05125@gmail.com>
2024-04-17 22:12:44 -0300

    onTouchStart, onTouchEnd

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View
+        onTouchStart={(event) => {
+          Alert.alert("TOUCH", "Started clicking");
+        }}
+        onTouchEnd={(evento) => {
+          Alert.alert("TOUCH", "Finished touching");
+        }}
+      >
+        <Text>Hello</Text>
+      </View>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
